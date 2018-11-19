52760413ca997540eb3fd754f2b107a5ac8545f5
yeonggilee <kwa03065@gmail.com>
2018-11-20 00:39:24 +0900

    Updates

---
+++ precache-manifest.2d48e2674e9ff57536d8d169c36244d2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "98d3695f82e55517af57",
+    "url": "/todo_list/static/js/runtime~main.98d3695f.js"
+  },
+  {
+    "revision": "dfce21d5053a3d8189cb",
+    "url": "/todo_list/static/js/main.dfce21d5.chunk.js"
+  },
+  {
+    "revision": "875bae81b5a055c285c6",
+    "url": "/todo_list/static/js/1.875bae81.chunk.js"
+  },
+  {
+    "revision": "dfce21d5053a3d8189cb",
+    "url": "/todo_list/static/css/main.1198a959.chunk.css"
+  },
+  {
+    "revision": "d42856f0b8fd70aa80406f33a7e6ac05",
+    "url": "/todo_list/index.html"
+  }
+];
